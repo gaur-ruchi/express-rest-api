cd0dc28d16f0302a2e60c675ab0e4f2bed51a643
gaur-ruchi <ruchigaur1993@gmail.com>
2026-07-07 18:52:34 +0000

    changes v6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server! This is a Node.js application running on port 8080.');
 });
 
 app.listen(PORT, () => {
